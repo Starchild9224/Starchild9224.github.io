04648ee881440449bf2e9f37019f9070d1b7cbf0
SayHiHelena <cuiziyuan99@gmail.com>
2025-04-01 11:30:22 +1100

    update hero head animation

--- scripts/hero.js
+++ scripts/hero.js
@@ -9,16 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function startTextAnimation() {
     const words = document.querySelectorAll(".sp-words-wrapper span");
+    const faces = document.querySelectorAll("#hero-face-container img");
     let currentIndex = 0;
 
     function showNextWord() {
         words[currentIndex].classList.remove("is-visible");
         words[currentIndex].classList.add("is-hidden");
+        faces[currentIndex].classList.remove("is-visible");
+        faces[currentIndex].classList.add("is-hidden");
 
         currentIndex = (currentIndex + 1) % words.length;
 
         words[currentIndex].classList.remove("is-hidden");
         words[currentIndex].classList.add("is-visible");
+        faces[currentIndex].classList.remove("is-hidden");
+        faces[currentIndex].classList.add("is-visible");
     }
 
     setInterval(showNextWord, 3000);
